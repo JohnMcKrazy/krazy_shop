d7446470fb237889febf5829198a4ca70c5b314d
John McKrazy <gc.john.dcg@gmail.com>
2024-02-10 20:19:56 -0600

    First Commit

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const selector = (tag) => document.querySelector(`${tag}`);
+const selectorAll = (tag) => document.querySelectorAll(`${tag}`);
+
+const themeBtn = selector(".theme_btn");
+const menuBtn = selector(".menu_btn");
+const BODY = selector("BODY");
+
+themeBtn.addEventListener("click", () => {
+    console.log("click");
+    if (BODY.className === "dark_theme") {
+        BODY.className = "light_theme";
+    } else {
+        BODY.className = "dark_theme";
+    }
+});
+let menuState = false;
+const animationIn = (item, delay = 500) => {
+    item.animate([{ transform: `translateX(-100%)` }, { transform: `translateX(0)` }], { duration: delay, fill: `forwards` });
+};
+const animationOut = (item, delay = 500) => {
+    item.animate([{ transform: `translateX(0)` }, { transform: `translateX(-100%)` }], { duration: delay, fill: `forwards` });
+};
+const navMenu = selector(".nav_menu");
+const menuAction = () => {
+    if (!menuState) {
+        menuState = true;
+        animationIn(navMenu);
+    } else {
+        animationOut(navMenu);
+        menuState = false;
+    }
+};
+menuBtn.addEventListener("click", () => {
+    menuAction();
+});
+
+selector(".contact_btn").addEventListener("click", () => {
+    menuAction();
+});
